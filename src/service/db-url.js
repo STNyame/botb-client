47c8680cf0e064667bad08f233ed2e900363dd03
STNyame <sentwalidors@hotmail.com>
2021-07-09 14:44:35 +0200

    change db_url

--- src/service/db-url.js
+++ src/service/db-url.js
@@ -1,2 +1,2 @@
-// export const DB_URL = "https://botb-server.herokuapp.com";
-export const DB_URL = "http://localhost:4000";
+export const DB_URL = "https://project-botb.herokuapp.com";
+// export const DB_URL = "http://localhost:4000";
